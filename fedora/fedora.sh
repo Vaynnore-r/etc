2a295e334d3de0520feed3d579d682d4c45451e3
Vaynnore <268145842+Vaynnore-r@users.noreply.github.com>
2026-05-12 23:07:55 +0200

    fixed fedora script

--- fedora/fedora.sh
+++ fedora/fedora.sh
@@ -1,16 +1,19 @@
-nmtui
-
 sudo dnf update -y
 
-sudo dnf install https://rpmfusion.org(rpm -E %fedora).noarch.rpm https://rpmfusion.org(rpm -E %fedora).noarch.rpm -y
+sudo dnf install -y \
+https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda mokutil kmodtool openssl -y
+sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda mokutil kmodtool openssl
 
-sudo kmodgenca
+sudo kmodgenca -a
 
 sudo mokutil --import /etc/pki/akmods/certs/public_key.der
 
 sudo grubby --update-kernel=ALL --args="nvidia-drm.modeset=1"
 
-sudo reboot
+# after
+#sudo akmods --force
+#sudo dracut --force
+#sudo reboot
 
